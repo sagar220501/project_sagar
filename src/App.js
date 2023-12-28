1bf225b91e4b42e476fc71c7a959d8167a389d08
sagar220501 <schalageri10@gmail.com>
2023-12-28 21:54:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+// App.js
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './pages/Home';
 import Clothes from './pages/Clothes';
@@ -8,57 +9,30 @@ import ProductInfo from './pages/ProductInfo';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cards from './pages/Cards';
-import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 
 function App() {
-  const navigate = useNavigate();
+  const [cookies] = useCookies(['jwt']);
 
   return (
     <Routes>
-      <Route path="/" element={<Login />} /> {/* Redirect to Login */}
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
-      <Route
-        path="/home"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
-      <Route
-        path="/clothes"
-        element={
-          <Layout>
-            <Clothes />
-          </Layout>
-        }
-      />
-      <Route
-        path="/product-detail/:product_id"
-        element={
-          <Layout>
-            <ProductInfo />
-          </Layout>
-        }
-      />
-      <Route
-        path="/cart"
-        element={
-          <Layout>
-            <Cart />
-          </Layout>
-        }
-      />
-      <Route
-        path="/cards"
-        element={
-          <Layout>
-            <Cards />
-          </Layout>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        {cookies.jwt ? (
+          <>
+            <Route path="/home" element={<Home />} />
+            <Route path="/clothes" element={<Clothes />} />
+            <Route path="/product-detail/:product_id" element={<ProductInfo />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/cards" element={<Cards />} />
+          </>
+        ) : (
+          // Redirect to login page if JWT doesn't exist
+          <Navigate to="/login" />
+        )}
+      </Route>
     </Routes>
   );
 }
